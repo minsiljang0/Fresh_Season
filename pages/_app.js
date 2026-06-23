b5f4f53f45b0964cac04f2c15e7964797b5b4858
minsiljang0 <minsiljang0@gmail.com>
2026-06-24 03:11:38 +0900

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,12 @@ export default function App({ Component, pageProps }) {
         <meta name="google-site-verification" content="Zbs0vFqBSKLZDlCi2dcR7SpDWfq7UYsTkYo6Dl_Y4QM" />
         <meta name="naver-site-verification" content="5e03e027756e4b86169b64785e34e7ac858ab380" />
         <meta name="yandex-verification" content="90aa2aa4b51918aa" />
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2161169464776476"
+          crossOrigin="anonymous"
+        />
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-33DGQKES2B" />
         <script dangerouslySetInnerHTML={{
