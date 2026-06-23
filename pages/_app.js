8a29cba11a8084688724e699068e8978adfdc24f
minsiljang0 <minsiljang0@gmail.com>
2026-06-23 21:12:56 +0900

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,16 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="google-site-verification" content="Zbs0vFqBSKLZDlCi2dcR7SpDWfq7UYsTkYo6Dl_Y4QM" />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-33DGQKES2B" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-33DGQKES2B');
+          `
+        }} />
       </Head>
       <Component {...pageProps} />
     </>
