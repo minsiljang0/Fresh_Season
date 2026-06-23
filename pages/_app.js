9629059394624e818a944e2dc8a6440e7fa09439
minsiljang0 <minsiljang0@gmail.com>
2026-06-23 20:59:23 +0900

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="Zbs0vFqBSKLZDlCi2dcR7SpDWfq7UYsTkYo6Dl_Y4QM" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
