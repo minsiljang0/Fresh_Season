08248672533ac38baadcab87e8a5c5b87671b5d5
minsiljang0 <minsiljang0@gmail.com>
2026-06-24 02:05:29 +0900

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="google-site-verification" content="Zbs0vFqBSKLZDlCi2dcR7SpDWfq7UYsTkYo6Dl_Y4QM" />
+        <meta name="naver-site-verification" content="5e03e027756e4b86169b64785e34e7ac858ab380" />
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-33DGQKES2B" />
         <script dangerouslySetInnerHTML={{
