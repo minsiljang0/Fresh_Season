a6ef5265807d0457e6cdd803902073d1d21f4c31
minsiljang0 <minsiljang0@gmail.com>
2026-06-26 04:26:09 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: '/sitemap.xml', destination: '/api/sitemap.xml' },
+      { source: '/robots.txt',  destination: '/api/robots.txt'  },
+    ]
+  },
 }
 module.exports = nextConfig
